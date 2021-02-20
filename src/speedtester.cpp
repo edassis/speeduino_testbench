119f076bdee4da4aa81a0a7d1992e7c6f605d936
Eduardo Assis <eduardoffassis@gmail.com>
2021-02-19 21:05:38 -0300

    Substitutes 'delay()' func for 'micros()'
    Uses 'micros()' to calculate clock temporization of 'RPM' and 'CAM'
    sensors.
    
    - Fixes outputs when above 2000 RPM.
    - General improvement in code quality.

--- src/speedtester.cpp
+++ src/speedtester.cpp
@@ -1,50 +1,85 @@
 #include <Arduino.h>
 
-int RPM_MIN = 200;
+bool DEBUG = false;
+
+unsigned LOOP_TIME = 33 * 1e3; // 30fps ~= 33 * 1000 us
+
+int POT_PIN = A0;   // select the input pin for the potentiometer
+int RPM_PIN = 2;    // select the pin for the RPM signal
+int PHASE_PIN = 4;  // select the pin for phase signal
+
+int CYL_QTD = 6;    // Engine cylinders quantity
 int RPM_MAX = 6000;
-int LOOP_TIME = 33; // 30fps ~= 33 ms
+int RPM_MIN = 200;
 
-int potPin = A0;  // select the input pin for the potentiometer
-int RPMPin = 2;   // select the pin for the LED
-int phasePin = 4; // select the pin for phase signal
+int rpm_state = LOW;
+int phase_state = LOW;
 
-int phase_counter = 0;
+unsigned long current_time = 0;
+unsigned long previous_serial_time = 0;
+unsigned long previous_rpm_time = 0;
+unsigned long previous_phase_time = 0;
 
 void setup()
 {
-  Serial.begin(9600);      // initialize serial communication at 9600 bits per second:
-  pinMode(RPMPin, OUTPUT); // declare the ledPin as an OUTPUT
-  pinMode(phasePin, OUTPUT);
+  if (DEBUG)
+    Serial.begin(9600); // initialize serial communication at 9600 bits per second
+
+  pinMode(RPM_PIN, OUTPUT); // declare the ledPin as an OUTPUT
+  pinMode(PHASE_PIN, OUTPUT);
 }
 
 void loop()
 {
-  int val = analogRead(potPin); // read the value from the sensor
-
-  int rpm = map(val, 0, 1023, RPM_MIN, RPM_MAX);
-
-  int wait = 1000 / (2 * 3 * rpm / 60); // time to wait for between clock`s edges
-                                        // 2 (high/low) * 3 (distributor pulses for 360 crank degrees) * rpm / 60 (seconds)
-
-  Serial.print(val);
-  Serial.print(" ");
-  Serial.print(rpm);
-  Serial.print(" ");
-  Serial.print(wait);
-  Serial.print(" ");
-  Serial.println(phase_counter);
-
-  digitalWrite(RPMPin, HIGH);
-  if (phase_counter == 6)
+  unsigned long temp = micros();
+  
+  if (temp < current_time) // overflow
   {
-    digitalWrite(phasePin, HIGH);
-    phase_counter = 0;
+    previous_serial_time = 0;
+    previous_rpm_time = 0;
+    previous_phase_time = 0;
   }
-  delay(wait);
+  current_time = temp;
 
-  digitalWrite(RPMPin, LOW);
-  digitalWrite(phasePin, LOW);
-  delay(wait);
+  unsigned val = analogRead(POT_PIN); // read the value from the sensor
 
-  phase_counter++;
+  unsigned rpm = map(val, 0, 1023, RPM_MIN, RPM_MAX);
+
+  unsigned long rpm_wait = 1e6 / (2 * (CYL_QTD / 2) * rpm / 60); // time to wait for between clock`s edges in microseconds
+                                                                 // 2 (high/low) * 3 (distributor pulses for one crankshaft revolution) * rpm / 60 (seconds)
+  unsigned long phase_wait = 1e6 / (2 * rpm / 60);
+
+  // Serial output
+  if (DEBUG && (current_time - previous_serial_time >= LOOP_TIME) )
+  {
+    previous_serial_time = current_time;
+
+    Serial.print("Potentiometer: ");
+    Serial.print(val);
+    Serial.print("\tRPM: ");
+    Serial.print(rpm);
+    Serial.print("\trpm_wait: ");
+    Serial.print(rpm_wait);
+    Serial.print("\tphase_wait: ");
+    Serial.print(phase_wait);
+    Serial.println();
+  }
+
+  // RPM output
+  if (current_time - previous_rpm_time >= rpm_wait)
+  {
+    previous_rpm_time = current_time;
+
+    rpm_state = !rpm_state;
+    digitalWrite(RPM_PIN, rpm_state);
+  }
+
+  // Crankshaft phase output
+  if (current_time - previous_phase_time >= phase_wait)
+  {
+    previous_phase_time = current_time;
+
+    phase_state = !phase_state;
+    digitalWrite(PHASE_PIN, phase_state);
+  }
 }
